a2a21e6c75f3ccb0f8aa3cffea96969db9676ff6
Alejandra Jurado <ajurado2204@gmail.com>
2016-02-08 19:36:00 -0500

    initialized server.js

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
  * Created by Ale on 2/8/16.
  */
 var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+var port = 8080;
+
+
+app.listen(port, function(){
+  console.log("Listening");
+});
