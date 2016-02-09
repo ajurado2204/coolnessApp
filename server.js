3a387d7a33efca3191ca2731ba3c225f178e2709
alemjc <alemjc2204@gmail.com>
2016-02-08 19:37:09 -0500

    added mysql connection object.

--- server.js
+++ server.js
@@ -4,8 +4,14 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
-
 var port = 8080;
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
 
 
 app.listen(port, function(){
