bab5563483f3d9e59e97159a88871c5c95f550f5
Alejandra Jurado <ajurado2204@gmail.com>
2016-02-08 19:50:12 -0500

    finished to routes

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ var app = express();
 
 var port = 8080;
 
+app.get('/cast', function(res, req){
+  connection.query("SELECT name FROM cast_table ORDER BY id ASC", function(err, result) {
+    res.send(result);
+  });
+});
+
+app.get('/attitude-chart/:type' , function(res,req){
+  var type = req.params.type;
+  connection.query("SELECT name FROM cast_table WHERE attitude = ?",type, function(err,result){
+    res.send(result);
+  });
+})
 
 app.listen(port, function(){
   console.log("Listening");
 });
+
